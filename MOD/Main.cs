a035f8f30f1639253076c45a041485fc90fa2436
McSkinnerOG <40324085+McSkinnerOG@users.noreply.github.com>
2022-05-08 16:41:01 +1000

    Template Code
    Initial Template setup

--- MOD/Main.cs
+++ MOD/Main.cs
@@ -21,6 +21,73 @@ namespace MOD
             log = Logger; harmony = new Harmony(GUID);
             assembly = Assembly.GetExecutingAssembly();
             modFolder = Path.GetDirectoryName(assembly.Location);
-        }  
+        }
+
+
+        public static PlayerMovement lp_movement;
+        public static PlayerInput lp_input;
+        public static PlayerManager lp_pmanager;
+        public static PlayerStatus lp_pstatus;
+        public static HitableActor lp_hittable;
+        public static DetectInteractables lp_interact; 
+        public static PingController lp_pingController;
+
+        public static PlayerMovement op_movement;
+        public static PlayerInput op_input;
+        public static PlayerManager op_pmanager;
+        public static PlayerStatus op_pstatus;
+        public static HitableActor op_hittable;
+        public static DetectInteractables op_interact;
+        public static PingController op_pingController;
+
+
+
+        //All
+        public static PlayerStatus[] All_Player_Status;
+        public static OnlinePlayer[] Player_List;
+
+        public void GetPlayerByID(int id)
+        {
+            Player_List = FindObjectsOfType<OnlinePlayer>();
+            foreach (OnlinePlayer op in Player_List)
+            { 
+                op_pmanager = op.GetComponent<PlayerManager>();
+                if (op_pmanager.id == id)
+                {
+                    //do stuff
+                    op_hittable = op.GetComponent<HitableActor>();
+                    op_pmanager = op.GetComponent<PlayerManager>();
+                    return;
+                }
+            }
+        } 
+        public void OnlinePlayer_Search()
+        {
+            Player_List = FindObjectsOfType<OnlinePlayer>();
+            foreach (OnlinePlayer op in Player_List)
+            {
+                op_hittable = op.GetComponent<HitableActor>();
+                op_pmanager = op.GetComponent<PlayerManager>();
+            }
+        }
+        public void LocalPlayer_Search()
+        {
+            if (lp_movement == null)
+            {
+                lp_movement = FindObjectOfType<PlayerMovement>();
+                lp_input = FindObjectOfType<PlayerInput>();
+
+                lp_pstatus = FindObjectOfType<PlayerStatus>();
+                lp_hittable = FindObjectOfType<HitableActor>();
+                lp_interact = FindObjectOfType<DetectInteractables>();
+                lp_pingController = FindObjectOfType<PingController>();
+            }
+            if (lp_pmanager == null) { lp_pmanager = lp_movement.gameObject.GetComponent<PlayerManager>(); }
+        }
+
+        public void Update()
+        {
+
+        } 
     }
 } 
