8e8c7dcaae1cb4e1e6033e0acc19052815644c79
McSkinnerOG <40324085+McSkinnerOG@users.noreply.github.com>
2022-05-08 21:11:41 +1000

    Hack setup

---
+++ MOD/MODULES/ONE_HIT.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+namespace MOD.MODULES
+{
+    public class ONE_HIT : MonoBehaviour
+    {
+        public static int time = 0;
+        public static void FixedUpdate()
+        {
+            time++;
+            Debug.Log(time);
+            if (time >= 100)
+            { 
+                OneHit();
+                time = 0;
+                return;
+            } 
+        } 
+        public static void OneHit()
+        {
+            var x = FindObjectsOfType<LootContainerInteract>();
+            var t = FindObjectsOfType<HitableTree>();
+            var m = FindObjectsOfType<HitableMob>();
+            var r = FindObjectsOfType<HitableRock>();
+            var a = FindObjectsOfType<HitableActor>();
+            foreach (LootContainerInteract item in x)
+            {
+                item.basePrice = 0;
+            }
+            foreach (HitableTree item in t)
+            {
+                item.hp = 1;
+            }
+            foreach (HitableMob item in m)
+            {
+                item.hp = 1;
+            }
+            foreach (HitableRock item in r)
+            {
+                item.hp = 1;
+            }
+            foreach (HitableActor item in a)
+            {
+                item.hp = 1; 
+            }
+            Debug.Log("FINISHED ONE HIT FUNCTION CORRECTLY");
+        } 
+    }
+}
--- MOD/Main.cs
+++ MOD/Main.cs
@@ -1,8 +1,11 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
 using System.IO;
-using System.Reflection; 
+using System.Reflection;
+using UnityEngine;
+
 namespace MOD
 {
     [BepInPlugin(GUID, MODNAME, VERSION)]
@@ -15,7 +18,7 @@ namespace MOD
         internal readonly Assembly assembly;
         public readonly string modFolder;
         #endregion
-        public void Start() { harmony.PatchAll(assembly); }
+        public void Start() { harmony.PatchAll(assembly); InitConfig(); }
         public Main()
         {
             log = Logger; harmony = new Harmony(GUID);
@@ -23,7 +26,7 @@ namespace MOD
             modFolder = Path.GetDirectoryName(assembly.Location);
         }
 
-
+        #region[LOCALPLAYER]
         public static PlayerMovement lp_movement;
         public static PlayerInput lp_input;
         public static PlayerManager lp_pmanager;
@@ -31,7 +34,8 @@ namespace MOD
         public static HitableActor lp_hittable;
         public static DetectInteractables lp_interact; 
         public static PingController lp_pingController;
-
+        #endregion
+        #region[ONLINEPLAYER]
         public static PlayerMovement op_movement;
         public static PlayerInput op_input;
         public static PlayerManager op_pmanager;
@@ -39,9 +43,7 @@ namespace MOD
         public static HitableActor op_hittable;
         public static DetectInteractables op_interact;
         public static PingController op_pingController;
-
-
-
+        #endregion 
         //All
         public static PlayerStatus[] All_Player_Status;
         public static OnlinePlayer[] Player_List;
@@ -75,8 +77,7 @@ namespace MOD
             if (lp_movement == null)
             {
                 lp_movement = FindObjectOfType<PlayerMovement>();
-                lp_input = FindObjectOfType<PlayerInput>();
-
+                lp_input = FindObjectOfType<PlayerInput>(); 
                 lp_pstatus = FindObjectOfType<PlayerStatus>();
                 lp_hittable = FindObjectOfType<HitableActor>();
                 lp_interact = FindObjectOfType<DetectInteractables>();
@@ -84,10 +85,40 @@ namespace MOD
             }
             if (lp_pmanager == null) { lp_pmanager = lp_movement.gameObject.GetComponent<PlayerManager>(); }
         }
-
+        public void FixedUpdate()
+        {
+            MODULES.ONE_HIT.FixedUpdate();
+        }
         public void Update()
         {
-
-        } 
+            
+            if (Input.GetKey(Heal1.Value)) { ClientSend.PlayerHit((int)DAMAGE_VALUE.Value, GameManager.players[0].id, 9f, 0, base.transform.position); }
+            if (Input.GetKey(Heal2.Value)) { ClientSend.PlayerHit(99, GameManager.players[1].id, 9f, 0, base.transform.position); }
+            if (Input.GetKey(Hurt1.Value)) { ClientSend.RevivePlayer(GameManager.players[0].id); }
+            if (Input.GetKey(Hurt2.Value)) { ClientSend.RevivePlayer(GameManager.players[LocalClient.instance.myId].id); }
+            if (Input.GetKey(KeyCode.C))
+            {
+                if (lp_movement == null) { LocalPlayer_Search(); }
+                lp_input.crouching = true;
+            }
+            if (Input.GetKeyUp(KeyCode.C))
+            {
+                if (lp_movement == null) { LocalPlayer_Search(); }
+                lp_input.crouching = false; 
+            } 
+        }
+        public static ConfigEntry<KeyCode> Heal1;
+        public static ConfigEntry<KeyCode> Heal2;
+        public static ConfigEntry<KeyCode> Hurt1;
+        public static ConfigEntry<KeyCode> Hurt2; 
+        public static ConfigEntry<float> DAMAGE_VALUE; 
+        public void InitConfig()
+        {
+            Heal1 = Config.Bind("Player", "Heal1", KeyCode.F1, "Apply Settings");
+            Heal2 = Config.Bind("Player", "Heal2", KeyCode.F2, "FREECAM");
+            Hurt1 = Config.Bind("Player", "Hurt1", KeyCode.F3, "FLIGHT");
+            Hurt2 = Config.Bind("Player", "Hurt2", KeyCode.F4, "FLIGHT");  
+            DAMAGE_VALUE = Config.Bind("Player", "DAMAGE", 999f, new ConfigDescription("Flight Speed", new AcceptableValueRange<float>(1f, 9999999f)));
+        }
     }
-} 
+}
