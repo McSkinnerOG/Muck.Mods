367da23cfbf69ce538636262d1d6fc39d713bda4
Mizar <43886709+Jwallzy@users.noreply.github.com>
2022-05-08 07:11:39 -0400

    Initial mizar commit
    My personal commit

--- MOD/Main.cs
+++ MOD/Main.cs
@@ -1,8 +1,12 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
 using System.IO;
-using System.Reflection; 
+using System.Reflection;
+using UnityEngine;
+using System.Collections;
+
 namespace MOD
 {
     [BepInPlugin(GUID, MODNAME, VERSION)]
@@ -31,7 +35,6 @@ namespace MOD
         public static HitableActor lp_hittable;
         public static DetectInteractables lp_interact; 
         public static PingController lp_pingController;
-
         public static PlayerMovement op_movement;
         public static PlayerInput op_input;
         public static PlayerManager op_pmanager;
@@ -39,6 +42,7 @@ namespace MOD
         public static HitableActor op_hittable;
         public static DetectInteractables op_interact;
         public static PingController op_pingController;
+        public static ImpactDamage Dmg;
 
 
 
@@ -76,18 +80,65 @@ namespace MOD
             {
                 lp_movement = FindObjectOfType<PlayerMovement>();
                 lp_input = FindObjectOfType<PlayerInput>();
-
                 lp_pstatus = FindObjectOfType<PlayerStatus>();
                 lp_hittable = FindObjectOfType<HitableActor>();
                 lp_interact = FindObjectOfType<DetectInteractables>();
                 lp_pingController = FindObjectOfType<PingController>();
+                Dmg = FindObjectOfType<ImpactDamage>();
             }
             if (lp_pmanager == null) { lp_pmanager = lp_movement.gameObject.GetComponent<PlayerManager>(); }
         }
 
+        public void Awake() { InitConfig(); }
+
+        public void FixedUpdate()
+        {
+            Modules.One_Hitter.FixedUpdate();
+        }
+
         public void Update()
         {
+            //Cheats
+            if (Input.GetKeyDown(KeycodeCheats.Value))
+            {
+                LocalPlayer_Search();
+                lp_pstatus.hp = 999;
+                lp_pstatus.maxHp = 999;
+                lp_pstatus.stamina = 99999;
+                lp_pstatus.maxStamina = 99999;
+                lp_pstatus.shield = 9999;
+                lp_pstatus.maxShield = 9999;
+                lp_pstatus.hunger = 9999;
+                lp_pstatus.currentSpeedArmorMultiplier = RunSpd.Value;
+            }
+            //Kill your teammate, revive yourself :)
+            if (Input.GetKey(Keycodekillu.Value)) { ClientSend.PlayerHit(9999,GameManager.players[1].id, 9999, 9999, base.transform.position); }
+            if (Input.GetKey(KeycodeRev.Value)) {  ClientSend.RevivePlayer(GameManager.players[0].id); }
+            if (Input.GetKeyDown(KeyCodeGetFucked.Value))
+            {
+                var m = FindObjectsOfType<HitableMob>();
+                foreach (HitableMob mob in m)
+                {
+                    mob.hp = 0;
+                }
+            }
+        }
 
-        } 
+        #region[Bepinex Config Entries]
+        public static ConfigEntry<KeyCode> KeycodeCheats;
+        public static ConfigEntry<KeyCode> Keycodekillu;
+        public static ConfigEntry<KeyCode> KeycodeRev;
+        public static ConfigEntry<KeyCode> KeyCodeGetFucked;
+        public static ConfigEntry<float> RunSpd;
+        #endregion 
+
+        public void InitConfig()
+        {
+            KeycodeCheats = Config.Bind("Cheats", "Infinite Hp, Shield, Stam, No Hunger", KeyCode.F1, "LMFAO!");
+            Keycodekillu = Config.Bind("Cheats", "Get sauced", KeyCode.F2, "Kill player");
+            KeycodeRev = Config.Bind("Cheatz", "Revive", KeyCode.F3, "Insta res");
+            KeyCodeGetFucked = Config.Bind("Cheats", "Hehe", KeyCode.F4, "Set Mobs hp to infinit");
+            RunSpd = Config.Bind("Run speed", "Run Speed Slider", 1f, new ConfigDescription("Running Speed", new AcceptableValueRange<float>(0f, 1500f)));
+        }
     }
 } 
---
+++ MOD/Modules/One Hitter.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+
+namespace MOD.Modules
+{
+    public class One_Hitter : MonoBehaviour
+    {
+        public static int time = 0;
+
+        public static void FixedUpdate()
+        {
+            time++;
+            Debug.Log(time);
+            if (time >= 145)
+            {
+                OneHit();
+                time = 0;
+                return;
+            }
+        }
+        public static void OneHit()
+        {
+            var x = FindObjectsOfType<LootContainerInteract>();
+            var t = FindObjectsOfType<HitableTree>();
+            var m = FindObjectsOfType<HitableMob>();
+            var r = FindObjectsOfType<HitableRock>();
+            var a = FindObjectsOfType<HitableActor>();
+            foreach (LootContainerInteract item in x)
+            {
+                item.basePrice = 0;
+            }
+            foreach (HitableTree item in t)
+            {
+                item.hp = 1;
+            }
+            foreach (HitableMob item in m)
+            {
+                item.hp = 1;
+            }
+            foreach (HitableRock item in r)
+            {
+                item.hp = 1;
+            }
+            foreach (HitableActor item in a)
+            {
+                item.hp = 1;
+            }
+            Debug.Log("Successfully applied onehitter!");
+        }
+    }
+}
