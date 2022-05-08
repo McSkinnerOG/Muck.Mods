65ba4f86a47310336b819a6897dbb11072699296
McSkinnerOG <40324085+McSkinnerOG@users.noreply.github.com>
2022-05-08 15:44:02 +1000

    Cleanup

--- MOD/Main.cs
+++ MOD/Main.cs
@@ -2,46 +2,26 @@
 using BepInEx.Logging;
 using HarmonyLib;
 using System.IO;
-using System.Reflection;
-
+using System.Reflection; 
 namespace MOD
 {
     [BepInPlugin(GUID, MODNAME, VERSION)]
     public class Main : BaseUnityPlugin
     {
-        #region[Declarations]
-
-        public const string
-            MODNAME = "MOD",
-            AUTHOR = "",
-            GUID = AUTHOR + "_" + MODNAME,
-            VERSION = "1.0.0.0";
-
+        #region[Declarations] 
+        public const string MODNAME = "MOD", AUTHOR = "", GUID = AUTHOR + "_" + MODNAME, VERSION = "1.0.0.0"; 
         internal readonly ManualLogSource log;
         internal readonly Harmony harmony;
         internal readonly Assembly assembly;
         public readonly string modFolder;
-
         #endregion
-
+        public void Start() { harmony.PatchAll(assembly); }
         public Main()
         {
-            log = Logger;
-            harmony = new Harmony(GUID);
+            log = Logger; harmony = new Harmony(GUID);
             assembly = Assembly.GetExecutingAssembly();
             modFolder = Path.GetDirectoryName(assembly.Location);
-        }
-
-        public void Start()
-        {
-            harmony.PatchAll(assembly);
-        }
-
-
-
-
-
-
-
+        } 
+        
     }
-}
+} 
