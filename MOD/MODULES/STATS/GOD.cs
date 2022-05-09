21cc1478bd74ddad242a62263608859a728e73d3
McSkinnerOG <40324085+McSkinnerOG@users.noreply.github.com>
2022-05-09 23:15:29 +1000

    Fixed value types causing errors
    Forgot to add some things because stoopid and fixed wrong value types

--- MOD/MODULES/STATS/GOD.cs
+++ MOD/MODULES/STATS/GOD.cs
@@ -12,23 +12,22 @@ namespace MOD.MODULES.STATS
     public class GOD : MonoBehaviour
     {
         public static bool god = false;
-        public static ConfigEntry<float> HP;
+        public static ConfigEntry<int> HP;
         public static ConfigEntry<KeyCode> KeyCode_Godmode;
         public static void Update()
         {
             if (SceneManager.GetActiveScene().name == "GameAfterLobby")
             {
                 if (Input.GetKeyDown(KeyCode_Godmode.Value)) { god = !god; }
-                if (god)
+                if (god == true)
                 {
                     if (Main.lp_pstatus != null)
                     {
                         // if our "lp_pstatus" reference can find required component 
                         // we set our "maxHp" equal to the sum of "hp + maxShield"
                         // hp being whatever our custom value is thanks to setting a new override for it with configs
-                        Main.lp_pstatus.hp = (int)HP.Value;
-                        Main.lp_pstatus.shield = Main.lp_pstatus.maxShield;
-                        Main.lp_pstatus.maxHp = (int)HP.Value + Main.lp_pstatus.maxShield;
+                        Main.lp_pstatus.hp = HP.Value; 
+                        Main.lp_pstatus.maxHp = HP.Value + (int)MODULES.STATS.STAMINA.StaminaMax.Value;
                     }
                     else { Main.LocalPlayer_Search(); }
                     // if our "lp_pstatus" reference can NOT find required component
